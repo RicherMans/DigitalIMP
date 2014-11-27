83444a0a175711118ed4771421b10fda6dbd9fb6
richermans <heini89@gmail.com>
2014-11-27 14:43:40 +0800

    Added Haar and Daubechies transform

--- PyProjects/Assignment7/__init__.py
+++ PyProjects/Assignment7/__init__.py
@@ -60,97 +60,101 @@ thresholdmask1 = np.array([
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0]
         ])
-h0 = np.array([1/np.sqrt(2),1/np.sqrt(2)])
-h1 = np.array([1/np.sqrt(2),-1/np.sqrt(2)])
-g0_daub = np.array([0.23037781,0.71484657,0.63088076,-0.02798376,-0.18703481,0.03084138,0.03288301,-0.0105940])
-g0_sym = np.array([0.0322,-0.0126,-0.0992,0.2979,0.8037,0.4976,-0.0296,-0.0758])
-mats = {'jpeg':jpegstd, 'zonal':zonal,'threshold':thresholdmask,'threshold1':thresholdmask1,'zonalbest':zonal_best}
-wavelets = {'haar':'a'}
+h0 = np.array([1 / np.sqrt(2), 1 / np.sqrt(2)])
+h1 = np.array([1 / np.sqrt(2), -1 / np.sqrt(2)])
+g0_daub = np.array([0.23037781, 0.71484657, 0.63088076, -0.02798376, -0.18703481, 0.03084138, 0.03288301, -0.0105940])
+g0_sym = np.array([0.0322, -0.0126, -0.0992, 0.2979, 0.8037, 0.4976, -0.0296, -0.0758])
+mats = {'jpeg':jpegstd, 'zonal':zonal, 'threshold':thresholdmask, 'threshold1':thresholdmask1, 'zonalbest':zonal_best}
 
 
-def encodewavelet(inputimage,waveletkernel,level):
-    b = np.dot(inputimage,waveletkernel)
+def encodewavelet(inputimage, waveletkernel, level):
+    ret = inputimage[:]
     for _ in range(level):
-        b = np.dot(b.T,waveletkernel)
-    return b
+        ret = np.dot(waveletkernel.T, np.dot(ret, waveletkernel))
+    return ret
+
+def decodewavelet(inputimage, waveletkernel, level):
+    ret = inputimage[:]
+    for _ in range(level):
+        ret = np.dot(waveletkernel, np.dot(ret, waveletkernel.T))
+    return ret
 
 def haarkernel(img):
-    m,_ = img.shape
-    upperhaar=[]
-    lowerhaar = []
-    for i in range(0,m,2):
-        q = np.zeros(m)
-        p = np.zeros(m)
-        q[i:i+len(h0)] = h0
-        p[i:i+len(h1)] = h1
-        upperhaar.append(q)
-        lowerhaar.append(p)
-    return np.vstack((upperhaar,lowerhaar)).T
+    m, _ = img.shape
+    return (np.vstack(((np.kron(np.eye(m / 2), h0)), (np.kron(np.eye(m / 2), h1)))).T)
 
-def daubechieswavelet(inputimage,waveletkernel):
-    m,_ = inputimage.shape
-    upperhalf = []
-    lowerhalf = []
-    for i in range(0,m,len(g0_daub)):
-        q = np.zeros(m)
-        p = np.zeros(m)
-        q[i:i+len(g0_daub)] =g0_daub
-#         Reverse
-        h0 = q[::-1]
-        g1 = -1*h0[::2]
-        for qq in range(h0):
-            if qq %2 == 1:
-                h0[qq] *= -1
-#         p[i:i+len(g0_daub)] =
-        
+'''
+Returns the forward and the backward wavelet
+'''
+def daubechieskernel(inputimage):
+    m, _ = inputimage.shape
+    h0 = g0_daub[::-1]
+    g1 = [h0[i] * -1. for i in range(len(g0_daub))]
+    h1 = g1[::-1] 
+    fwd = np.vstack((np.kron(np.eye(m / 2), h0), np.kron(np.eye(m / 2), h1)))
+    bwd = np.vstack((np.kron(np.eye(m / 2), g0_daub), np.kron(np.eye(m / 2), g1)))
+    return (fwd, bwd)
 
+wavelets = {'haar':haarkernel, 'daub':daubechieskernel}
 def main():
     args = parseArgs()
     if args.wavelet:
-        kernel = haarkernel(args.inputimage)
-        kernel = daubechieswavelet(args.inputimage,None)
-        discrete_haar = encodewavelet(args.inputimage,kernel,3)
-#         discrete_haar = discreteHaarWaveletTransform(args.inputimage)
-        misc.imsave(args.o +'_haar.tif',discrete_haar)
-#         print (np.eye(m/2, n) * h0).shape
-#         upperhaar = np.vstack((np.eye(m/2, n) * h0),(np.eye(m/2, n) * h1))
-#         print upperhaar
-    encodedimgs = encode(args.inputimage,mats[args.quantmattype])
-    decodedimg = decode(encodedimgs,args.inputimage.shape[0])
-    if args.o:
-        misc.imsave(args.o+'_reconstructed.tif',decodedimg)
-    difference = args.inputimage - decodedimg
-    if args.o:
-        misc.imsave(args.o+'_diff.tif',difference)
+        kernel = wavelets[args.wavelet](args.inputimage)
+#      if we return a tuple, we have two different functions for forward
+#     transform and backward transform
+        if args.wavelet == 'haar':
+            fwd = kernel
+            bwd = kernel
+        else:
+            fwd = kernel[0]
+            bwd = kernel[1]
+#         kernel = daubechieskernel(args.inputimage,None)
+        encoded = encodewavelet(args.inputimage, fwd, 3)
+        if args.o:
+            misc.imsave(args.o + '_' + args.wavelet + '_encoded.tif', encoded)
+#         Removes the less important compnents, yet makes the picture quality worse        
+        encoded[encoded < args.threshold] = 0
+        decoded = decodewavelet(encoded, bwd, 3)
+        if args.o:
+            misc.imsave(args.o + '_' + args.wavelet + '_reconstructed.tif', decoded)
+    if args.quantmattype:
+        encodedimgs = encode(args.inputimage, mats[args.quantmattype])
+        decodedimg = decode(encodedimgs, args.inputimage.shape[0])
+        if args.o:
+            misc.imsave(args.o + '_reconstructed.tif', decodedimg)
+        difference = args.inputimage - decodedimg
+        if args.o:
+            misc.imsave(args.o + '_diff.tif', difference)
 
 
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('inputimage', type=misc.imread)
-    parser.add_argument('-quantmattype', choices=mats, default=mats.values()[0])
-    parser.add_argument('-wavelet',choices=wavelets,default=wavelets.values()[0])
-    parser.add_argument('-o',type=str,help='Outputfile')
+    parser.add_argument('-quantmattype', choices=mats)
+    parser.add_argument('-wavelet', choices=wavelets)
+    parser.add_argument('-t', '--threshold', help='Tht Threshold for the wavelet transforms, all values in the encoded wavelet will be truncated below threshold',action='store_true')
+    parser.add_argument('-o', type=str, help='Outputfile')
     return parser.parse_args()
 
 
 
-def decode(encodedimgs,rowlength):
+def decode(encodedimgs, rowlength):
     inverseimgs = inversetransform(encodedimgs)
-    mergedimg = mergesubimages(inverseimgs,rowlength)
+    mergedimg = mergesubimages(inverseimgs, rowlength)
     return mergedimg
 #     return readjustrange(mergedimg)
     
-def mergesubimages(subimages,rowlength):
-    ret = np.empty((rowlength,rowlength))
+def mergesubimages(subimages, rowlength):
+    ret = np.empty((rowlength, rowlength))
 #     Actuially its 64 here
-    for j in range(rowlength/(len(subimages)/rowlength)):
-        for i in range(rowlength/(len(subimages)/rowlength)):
-            subimg= subimages[(j*rowlength/(len(subimages)/rowlength))+i]
+    for j in range(rowlength / (len(subimages) / rowlength)):
+        for i in range(rowlength / (len(subimages) / rowlength)):
+            subimg = subimages[(j * rowlength / (len(subimages) / rowlength)) + i]
             for q in range(len(subimg)):
-                a = j*len(subimg)+ q
+                a = j * len(subimg) + q
                 for p in range(len(subimg[0])):
                     b = i * len(subimg[0]) + p
-                    ret[a,b] = subimg[q,p]   
+                    ret[a, b] = subimg[q, p]   
     return ret
 #     return subimages.reshape((512,512))
     
@@ -162,7 +166,7 @@ def inversetransform(encodedimgs):
 
 def encode(rawimg, quantmat):
 #     adjustedimg = adjustrange(rawimg)
-    rawimg = np.array(rawimg,dtype=float)
+    rawimg = np.array(rawimg, dtype=float)
     subimages = np.array(list(constructsubimages(rawimg)))
     dcttransformedimgs = forwardtransform(subimages)
     quantimgs = quantitize(dcttransformedimgs, quantmat)
@@ -188,7 +192,7 @@ def forwardtransform(subimages):
 def quantitize(dcttransformedimgs, quantmat):
     quantized = []
     for dcttransform in dcttransformedimgs:
-        quantized.append(dcttransform*quantmat)
+        quantized.append(dcttransform * quantmat)
     return np.array(quantized)
 
 if __name__ == '__main__':
